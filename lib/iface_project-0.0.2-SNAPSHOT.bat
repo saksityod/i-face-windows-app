9edf0531ca963bf681b3cc175e99b5cc18195e43
GJ-DARIS <34049335+GJ-DARIS@users.noreply.github.com>
2019-03-04 14:51:40 +0700

    [Added]: เพิ่มไฟล์ .bat

---
+++ lib/iface_project-0.0.2-SNAPSHOT.bat
@@ -0,0 +1,4 @@
+
+:: Run iface_project-0.0.2-SNAPSHOT.jar
+  
+java -cp iface_project-0.0.2-SNAPSHOT.jar;lib/iface-connector-main-3.1.0.0.jar;lib/jna-4.5.0.jar;lib/jna-platform-4.5.0.jar;lib/sdk-commons-main-1.1.6.jar;lib/slf4j-api-1.7.25.jar;lib/vlcj-3.10.1.jar;lib/mysql-connector-java-5.1.21.jar;lib/slf4j-api-1.8.0-beta2;lib/commons-lang-2.6.jar;lib/lib_http/apache-httpcomponents-httpcore.jar;lib/lib_http/apache-httpcomponents-httpmime.jar;lib/lib_http/httpclient-4.0.3.jar;lib/commons-logging-1.2.jar;lib/joda-time-2.9.9.jar;lib/log4j-1.2.12.jar;lib/commons-codec-1.9.jar;lib/httpcore-4.4.4.jar; th.co.imake.rtsp.IFaceTech
